be9b4380facb108268aa0ea18c85e6e463c4a1ce
bbarr <brendanbarr.web@gmail.com>
2011-11-30 15:30:03 -0500

    new api

--- marker.js
+++ marker.js
@@ -1,3 +1,9 @@
+/**
+ *  Javascript Templating
+ *
+ *  @author Brendan Barr brendanbarr.web@gmail.com
+ */
+
 var Marker = (function() {
 	
 	var Template = function(fn) {
@@ -8,40 +14,59 @@ var Marker = (function() {
 	}
 
 	Template.prototype = {
-	
-		partial: function(name, data) {
-			var template = Marker.templates[name];
+
+		/**
+		 * Reuses an existing Marker template
+		 * @param name {String} The name of the template
+		 * @param 2..n {Arguments} A list of arguments to pass into the template
+		 */
+		partial: function(name) {
+		  
+			var template = (typeof name === 'function') ? name : Marker.templates[name];
 			if (!template) throw new Error('Template: ' + name + ' not found');
 
 			// hijack the active_fn so the partial builds itself right into the current template 
-			this.active_fn = template.fn;
-			this._construct(data);
+			// use either .fn property or the template itself we are given a funtion to call
+			this.active_fn = template.fn || template;
+			
+			var args = [].slice.call(arguments);
+			args.shift();
+			
+			this._construct.apply(this, args);
 			this.active_fn = this.fn;
 
 			return this;
 		},
-			
-		end: function() {
-	
+		
+		_: function() {
+
 			var last = this.stack.pop();
-	
-			if (!this.stack[0]) {
+
+			if (!this.stack[0] && last) {
 				this.storage.appendChild(last);
 			}
-	
+
 			return this;
 		},
-	
+
+		html: function(html) {
+		  
+			var el = this.stack[this.stack.length - 1];
+			this._append_html(el, text);
+
+			return this;
+		},
+
 		text: function(text) {
 	
 			var el = this.stack[this.stack.length - 1];
-			this._append_content(el, text);
-	
+			this._append_text(el, text);
+
 			return this;
 		},
-	
+
 		_elements: [],
-		
+	
 		_create_element: function(tag) {					
 
 			var els = this._elements;
@@ -49,11 +74,15 @@ var Marker = (function() {
 	
 			return els[tag].cloneNode(false);
 		},
-	
-		_append_content: function(el, text) {
+
+		_append_text: function(el, text) {
 			(/\&\S+;/.test(text)) ? el.innerHTML += text : el.appendChild(document.createTextNode(text));
 		},
-	
+
+		_append_html: function(el, html) {
+			el.innerHTML += text;
+		},
+
 		_append_styles: function(el, styles) {
 			
 			var name, style = el.style;
@@ -67,27 +96,27 @@ var Marker = (function() {
 			var attr, name;
 			for (name in attrs) {
 				attr = attrs[name];
-				if (name === 'cache') this.cache[attr] = el;
 				if (name === 'style') this._append_styles(el, attr);
+				else if (name === 'cache') this.cache[attr] = el;
 				else (typeof el[name] !== 'undefined') ? el[name] = attr : el.setAttribute(name, attr);
 			}
 		},
-    
-    _place: function(el) {
-	
+
+		_place: function(el) {
+
 			if (this.stack[0]) {
 				this.stack[this.stack.length - 1].appendChild(el);
 			}
 			else {
 				this.storage.appendChild(el);
 			}
-	
+
 			this.stack.push(el);
 		},
-    
-		_to_html: function(data) {
+
+		_to_html: function() {
 			
-			this._construct(data);
+			this._construct.apply(this, [].slice.call(arguments));
 			
 			var html = this.storage.cloneNode(true);
 			this.storage = document.createDocumentFragment();
@@ -95,14 +124,14 @@ var Marker = (function() {
 			return html;
 		},
 
-		_construct: function(data) {
+		_construct: function() {
 
 			var current_stack_count = this.stack.length;
 	
-			this.active_fn(data);
+			this.active_fn.apply(this, [].slice.call(arguments));
 
 			while (this.stack[current_stack_count]) {
-				this.end();
+				this._();
 			}
 		}
 	};
@@ -111,38 +140,48 @@ var Marker = (function() {
 
 		var tags = [
 			'p','h1','h2','h3','h4','h5','h6','strong','em','abbr','address','bdo','blockquote','cite','q','code','ins','del','dfn','kbd','pre','samp','var','br',
-			'div', 'span', 'section', 'header', 'footer', 'sidebar',
+			'div', 'span', 'section', 'header', 'footer', 'sidebar', 'sub', 'sup',
 			'a', 'base',
-			'img','area','map','object','param',
+			'img','area','map','object','param', 'canvas',
 			'ul','ol','li','dl','dt','dd',
 			'table','tr','td','th','tbody','thead','tfoot','col','colgroup','caption',
 			'form','input','textarea','select','option','optgroup','button','label','fieldset','legend'
-	    	    ],
-		    generate = function(tag) {
+	  ],
+	  proto = Template.prototype,
+		generate = function(tag) {
 	
-			Template.prototype[tag] = function(attrs, content) {
+			proto[tag] = function(attrs, text, has_children) {
 	
 				var el = this._create_element(tag),
 				    attrs_type = typeof attrs;
 	
-				if (content) {
-					this._append_content(el, content);
+				if (text) {
+					this._append_text(el, text);
 				}
 	
 				if (attrs_type == 'string' || attrs_type == 'number') {
-					this._append_content(el, attrs);
+					this._append_text(el, attrs);
 				}
 				else {
 					this._append_attributes(el, attrs);
 				}
 	
 				this._place(el);
+				
+				if (!has_children) {
+				  this._();
+				}
 	
 				return this;
-		  }
-		  },
-		    len = tags.length,
-		    i = 0;
+		  };
+		  
+		  proto[tag + '_'] = function(attrs) {
+        this[tag](attrs, null, true);
+        return this;
+		  };
+		},
+		len = tags.length,
+		i = 0;
 	
 		for (; i < len; i++) {
 			generate(tags[i]);
@@ -154,15 +193,27 @@ var Marker = (function() {
 		templates: {},
 		
 		register: function(name, fn) {
+
 			var template = this.templates[name];
 			if (template) throw new Error('Template: ' + name + ' already exists');
-			return this.templates[name] = new Template(fn);
+
+			this.templates[name] = new Template(fn);
 		},
 		
-		render: function(name, data) {
+		render: function(name) {
+
 			var template = this.templates[name];
 			if (!template) throw new Error('Template: ' + name + ' not found');
-			return template._to_html(data);
+
+			var args = [].slice.call(arguments);
+			args.shift();
+			
+			var html = template._to_html.apply(template, args);
+			
+			return {
+			  html: (html.childNodes.length === 1) ? html.childNodes[0] : html,
+			  cache: template.cache
+			}
 		}
 	}
 })();
